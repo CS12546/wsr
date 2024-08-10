cc5028cde1e56a37f5b7669d1daf2b90cccf66f9
Cristopher Andrés Sáez Araneda <cristopher.saez2001@alumnos.ubiobio.cl>
2024-08-10 14:46:02 -0400

    in

---
+++ BotoneraAndroid/app/src/main/java/com/example/botoneraandroid/Dispositivos.java
@@ -0,0 +1,13 @@
+package com.example.botoneraandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dispositivos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dispositivos);
+    }
+}
